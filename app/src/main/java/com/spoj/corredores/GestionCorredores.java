d4ed93dc853c4c6e966b5c79f9fd5521af42822c
PaulOrmaza <steven.ormaza816@ist17dejulio.edu.ec>
2025-01-06 20:16:05 -0500

    Creacion de proyecto para servicio web de carrera en android

---
+++ app/src/main/java/com/spoj/corredores/GestionCorredores.java
@@ -0,0 +1,14 @@
+package com.spoj.corredores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GestionCorredores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_corredores);
+    }
+}
---
+++ app/src/main/java/com/spoj/corredores/MainActivity.java
@@ -0,0 +1,36 @@
+package com.spoj.corredores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            Button btnGestionCorredores = findViewById(R.id.btn_gestion_corredores);
+            Button btnSimularCarrera = findViewById(R.id.btn_simular_carrera);
+
+            // Navegar a la gestión de corredores
+            btnGestionCorredores.setOnClickListener(v -> {
+                Intent intent = new Intent(MainActivity.this, GestionCorredores.class);
+                startActivity(intent);
+            });
+
+            // Navegar a la simulación de carrera
+            btnSimularCarrera.setOnClickListener(v -> {
+                Intent intent = new Intent(MainActivity.this, Carrera.class);
+                startActivity(intent);
+            });
+        }
+    }
+
